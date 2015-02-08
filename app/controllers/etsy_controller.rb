282579b687481ca42b5e8490a2d9cfda9ec68737
William Jones <williammasonjones@gmail.com>
2015-02-08 15:49:58 -0500

    render search view

--- app/controllers/etsy_controller.rb
+++ app/controllers/etsy_controller.rb
@@ -1,2 +1,9 @@
 class EtsyController < ApplicationController
+	#ETSY_API_TOKEN = ENV['ETSY_API_TOKEN']
+	# ETSY_API_TOKEN = lfdnd7eyx7gsr5sphbowaren
+	# SECRET = o24rht1fv9
+
+	def search
+		render :search
+	end
 end
