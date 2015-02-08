3c2c17dc415477b81b0d11114582ceb72300567b
William Jones <williammasonjones@gmail.com>
2015-02-08 17:40:26 -0500

    added search method

--- app/controllers/etsy_controller.rb
+++ app/controllers/etsy_controller.rb
@@ -1,9 +1,13 @@
 class EtsyController < ApplicationController
-	#ETSY_API_TOKEN = ENV['ETSY_API_TOKEN']
+	ETSY_API_TOKEN = ENV['ETSY_API_TOKEN']
 	# ETSY_API_TOKEN = lfdnd7eyx7gsr5sphbowaren
 	# SECRET = o24rht1fv9
 
 	def search
+	  if params[:q]
+	  	response = Etsy::Request.get('/listings/active', :includes => ['Images', 'Shop'], :keywords => params[:q])
+	  	@results = JSON.parse(response.body)['results']
+	  end
 		render :search
 	end
 end
