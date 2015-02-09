74fa1f84116443dd505409fca0da08bfb4edbf52
William Jones <williammasonjones@gmail.com>
2015-02-09 18:16:19 -0500

    clean up

--- app/controllers/etsy_controller.rb
+++ app/controllers/etsy_controller.rb
@@ -1,8 +1,4 @@
 class EtsyController < ApplicationController
-	# ETSY_API_TOKEN = ENV['ETSY_API_TOKEN']
-	# ETSY_API_TOKEN = lfdnd7eyx7gsr5sphbowaren
-	# SECRET = o24rht1fv9
-
 	def search
 	  if params[:q]
 	  	response = Etsy::Request.get('/listings/active', :includes => ['Images', 'Shop'], :keywords => params[:q])
