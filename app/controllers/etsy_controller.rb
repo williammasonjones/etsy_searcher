44e6205b0b587d920a369420746d2e0163771212
William Jones <williammasonjones@gmail.com>
2015-02-12 18:18:34 -0500

    clean up

--- app/controllers/etsy_controller.rb
+++ app/controllers/etsy_controller.rb
@@ -4,7 +4,6 @@ class EtsyController < ApplicationController
 	  	response = Etsy::Request.get('/listings/active', :includes => ['Images', 'Shop'], :keywords => params[:q])
 	  	@results = JSON.parse(response.body)['results']
 	  end
-	  # binding.pry
 		render :search
 	end
 end
